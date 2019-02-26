2254857af0debf6482853a8e8f62cf3edb080e3a
Michael Zöhling <Michael.Zoehling@outlook.com>
2019-02-26 14:48:58 +0100

    Konvertieren

--- HalloWelt/HalloWelt/Program.cs
+++ HalloWelt/HalloWelt/Program.cs
@@ -31,6 +31,63 @@ namespace HalloWelt
             // STRG + K + D -> Formatierung richten
             #endregion
 
+            #region Variablen
+            //int zahl1;          // Deklaration
+            //int zahl2 = 12;     // Deklaration + Initialisierung
+
+            //int z1, z2, z3;     // 3 Int werden erstellt
+            //int z4 = 5, z5 = 10, z6 = 100; // Deklaration + Initialisierung
+
+            //var meineZahl = 5.33333;  // Zuweisung bestimmt den Datentypen 
+            #endregion
+
+            // Snippet für Console.WriteLine
+            // cw + TAB + TAB
+
+            // Suffix für numerische Werte:
+            //      F: float
+            //      D: double
+            //      U: uint
+            //      L: long
+            //      UL: ulong
+            //      M:  decimal
+
+
+            #region Konvertieren
+            //// Implizit
+            //int zahl1 = 5;
+            //long zahl2 = 10;
+
+            //zahl2 = zahl1; // Implizit Konvertieren -> "der kleinere Wert passt in den größeren Wertebereich hinein"
+            //zahl2 = 50;
+
+            //// zahl1 = zahl2; // "der größere Wert passt nicht in den kleineren Wertebereich hinein"
+
+            //// Explizit
+            ////zahl1 = Convert.ToInt32("abcde123",System.Globalization.CultureInfo.CurrentCulture);
+            //zahl1 = Int32.Parse("123");
+
+            //// double zahl3 = Convert.ToDouble("12.5");
+            //// Console.WriteLine(zahl3);
+
+            //string eingabe = Console.ReadLine();
+            //Console.WriteLine("Deine Eingabe war: " + eingabe);
+
+            //// Mini-Übung:
+            //// "Addierer" : 2 Zahlen einlesen und die Summe ausgeben
+
+            //// Boxing
+            //// In C# ist alles ein object
+
+            //int i = 12;
+            //object o = i; // Boxing
+
+            //// (int) => Casting
+            //int meineZahl99 = (int)o; // Unboxing - operation
+
+            //Console.WriteLine(o.GetType()); 
+            #endregion
+
             Console.WriteLine("---ENDE---");
             Console.ReadKey();
         }
