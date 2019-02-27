890486dbd004e4ba0a866c2d676385b768c896f0
Michael Zöhling <Michael.Zoehling@outlook.com>
2019-02-27 09:50:00 +0100

    Felder

---
+++ OOP/OOP/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    // Begriffe:
+    // Klasse:          Der "Bauplan" - Definition, wie das Objekt aussehen soll
+    // Objekt/Instanz:  Die "Realisierung" der Klasse
+    // Feld:            Variable in einer Klasse
+
+    // Gültigkeitsspezifizierer:
+    // public:              -> Zugriff von überall aus möglich ist
+    // private:             -> Zugriff ist nur von innerhalb der eigenen Klasse aus möglich
+    //   private ist der Standardfall für Felder (Variablen)
+
+    class Person
+    {
+        public string Vorname;
+        public string Nachname;
+        public byte Alter;
+        public decimal Kontostand;
+        public bool Geschlecht;
+    }
+}
--- OOP/OOP/Program.cs
+++ OOP/OOP/Program.cs
@@ -10,6 +10,23 @@ namespace OOP
     {
         static void Main(string[] args)
         {
+            Person p1 = new Person(); // Instanzieren 
+            p1.Vorname = "Tom";
+            p1.Nachname = "Ate";
+            p1.Alter = 50;
+            p1.Kontostand = 500m;
+            p1.Geschlecht = true; // Männlich
+
+            Person p2 = new Person();
+            p2.Vorname = "Anna";
+            p2.Nachname = "Nass";
+
+            Console.WriteLine(p1.Vorname +  " " + p1.Nachname);
+
+            p1.Kontostand = -2000000m;
+
+            Console.WriteLine("---ENDE---");
+            Console.ReadKey();
         }
     }
 }
