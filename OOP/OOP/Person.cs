a9542195b92a03f2086197845ba48cb5f2414725
Michael Zöhling <Michael.Zoehling@outlook.com>
2019-02-27 10:00:08 +0100

    Properties ala "Java"

--- OOP/OOP/Person.cs
+++ OOP/OOP/Person.cs
@@ -10,6 +10,7 @@ namespace OOP
     // Klasse:          Der "Bauplan" - Definition, wie das Objekt aussehen soll
     // Objekt/Instanz:  Die "Realisierung" der Klasse
     // Feld:            Variable in einer Klasse
+    // Methode:         Funktionalität in der Klasse (Subprozedur oder Funktion)
 
     // Gültigkeitsspezifizierer:
     // public:              -> Zugriff von überall aus möglich ist
@@ -18,10 +19,32 @@ namespace OOP
 
     class Person
     {
-        public string Vorname;
-        public string Nachname;
-        public byte Alter;
-        public decimal Kontostand;
-        public bool Geschlecht;
+        #region Felder
+        //public string Vorname;
+        //public string Nachname;
+        //public byte Alter;
+        //public decimal Kontostand;
+        //public bool Geschlecht; 
+        #endregion
+
+        private byte alter;
+
+        // Methode: 
+        // sichtbarkeit rückgabe name (parameter)
+        // { .... }
+        // void == keine rückgabe (subprozedur)
+        public void AlterSetzen(byte neuesAlter)
+        {
+            if (neuesAlter > 150)
+                Console.WriteLine("Ungültiges Alter");
+            else
+                alter = neuesAlter;
+        }
+
+        // byte -> rückgabetyp
+        public byte AlterAuslesen()
+        {
+            return alter; // return -> wert zurückgeben als "Ergebnis" der Funktion
+        }
     }
 }
--- OOP/OOP/Program.cs
+++ OOP/OOP/Program.cs
@@ -10,20 +10,29 @@ namespace OOP
     {
         static void Main(string[] args)
         {
-            Person p1 = new Person(); // Instanzieren 
-            p1.Vorname = "Tom";
-            p1.Nachname = "Ate";
-            p1.Alter = 50;
-            p1.Kontostand = 500m;
-            p1.Geschlecht = true; // Männlich
+            #region Felder
+            //Person p1 = new Person(); // Instanzieren 
+            //p1.Vorname = "Tom";
+            //p1.Nachname = "Ate";
+            //p1.Alter = 50;
+            //p1.Kontostand = 500m;
+            //p1.Geschlecht = true; // Männlich
 
-            Person p2 = new Person();
-            p2.Vorname = "Anna";
-            p2.Nachname = "Nass";
+            //Person p2 = new Person();
+            //p2.Vorname = "Anna";
+            //p2.Nachname = "Nass";
 
-            Console.WriteLine(p1.Vorname +  " " + p1.Nachname);
+            //Console.WriteLine(p1.Vorname +  " " + p1.Nachname);
 
-            p1.Kontostand = -2000000m;
+            //p1.Kontostand = -2000000m; 
+            #endregion
+
+            Person p1 = new Person();
+            p1.AlterSetzen(12);
+            p1.AlterSetzen(55);
+            p1.AlterSetzen(255);
+
+            Console.WriteLine(p1.AlterAuslesen());
 
             Console.WriteLine("---ENDE---");
             Console.ReadKey();
