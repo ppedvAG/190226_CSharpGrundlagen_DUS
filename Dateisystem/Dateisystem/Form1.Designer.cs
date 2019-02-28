f9682f9da944dfd1cc948a69d55737c886b8ed0b
Michael Zöhling <Michael.Zoehling@outlook.com>
2019-02-28 16:43:32 +0100

    Dateisystem

---
+++ Dateisystem/Dateisystem/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace Dateisystem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.dateiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.speichernToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.öffnenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.beendenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBoxInhalt = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.dateiToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(732, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // dateiToolStripMenuItem
+            // 
+            this.dateiToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.speichernToolStripMenuItem,
+            this.öffnenToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.beendenToolStripMenuItem});
+            this.dateiToolStripMenuItem.Name = "dateiToolStripMenuItem";
+            this.dateiToolStripMenuItem.Size = new System.Drawing.Size(57, 24);
+            this.dateiToolStripMenuItem.Text = "Datei";
+            // 
+            // speichernToolStripMenuItem
+            // 
+            this.speichernToolStripMenuItem.Name = "speichernToolStripMenuItem";
+            this.speichernToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.speichernToolStripMenuItem.Text = "Speichern";
+            this.speichernToolStripMenuItem.Click += new System.EventHandler(this.speichernToolStripMenuItem_Click);
+            // 
+            // öffnenToolStripMenuItem
+            // 
+            this.öffnenToolStripMenuItem.Name = "öffnenToolStripMenuItem";
+            this.öffnenToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.öffnenToolStripMenuItem.Text = "Öffnen";
+            this.öffnenToolStripMenuItem.Click += new System.EventHandler(this.öffnenToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(213, 6);
+            // 
+            // beendenToolStripMenuItem
+            // 
+            this.beendenToolStripMenuItem.Name = "beendenToolStripMenuItem";
+            this.beendenToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.beendenToolStripMenuItem.Text = "Beenden";
+            this.beendenToolStripMenuItem.Click += new System.EventHandler(this.beendenToolStripMenuItem_Click);
+            // 
+            // textBox1
+            // 
+            this.textBoxInhalt.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBoxInhalt.Location = new System.Drawing.Point(0, 28);
+            this.textBoxInhalt.Multiline = true;
+            this.textBoxInhalt.Name = "textBox1";
+            this.textBoxInhalt.Size = new System.Drawing.Size(732, 422);
+            this.textBoxInhalt.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(732, 450);
+            this.Controls.Add(this.textBoxInhalt);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem dateiToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem speichernToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem öffnenToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem beendenToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBoxInhalt;
+    }
+}
+
---
+++ Dateisystem/Dateisystem/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Dateisystem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void speichernToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // 3 Varianten
+            #region Variante mit einem FileStream
+            //FileStream stream = new FileStream("demo.txt", FileMode.Create);
+            //// Zeichenkette zu einem Bytearray verarbeiten:
+            //byte[] data = Encoding.Default.GetBytes(textBoxInhalt.Text);
+            //stream.Write(data, 0, data.Length);
+            //stream.Flush();
+            //stream.Close(); 
+            #endregion
+
+            #region Variante mit einem StreamReader/Writer
+            //StreamWriter sw = new StreamWriter("demo.txt"); // Lesen: StreamReader
+            //sw.WriteLine(textBoxInhalt.Text);
+            //sw.Flush();
+            //sw.Close(); 
+            #endregion
+
+
+
+            // Ordner:
+            //Directory.GetFiles("C:\\")
+            //foreach (string file in Directory.GetFiles("C:\\")
+            //{
+            //    if(Path.GetExtension(file) == ".exe")
+
+            //}
+            //FolderBrowserDialog dlg = new FolderBrowserDialog();
+            //dlg.ShowDialog();
+
+            SaveFileDialog dlg = new SaveFileDialog();
+            dlg.Filter = "Textdokument | *.txt";
+
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                // Datein:
+                File.WriteAllText(dlg.FileName, textBoxInhalt.Text);
+                // Features:
+                // File.Exists
+                File.SetCreationTime(dlg.FileName, new DateTime(1856, 11, 11, 23, 52, 12));
+                MessageBox.Show("Datei wurde erfolgreich gespeichert !!!");
+            }
+        }
+
+        private void öffnenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Title = "Na welche Datei willst du denn öffnen ?????";
+            dlg.Filter = "Textdokument | *.txt";
+            if(dlg.ShowDialog() == DialogResult.OK)
+            {
+                textBoxInhalt.Text = File.ReadAllText(dlg.FileName);
+            }
+        }
+
+        private void beendenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // 1) Close()
+            Close();
+            // 2) Allgemein:
+            // Environment.Exit(0);
+        }
+    }
+}
