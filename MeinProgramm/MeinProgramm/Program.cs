4bceb70fab4dcb178b97528af6c53d3ed22cff94
Michael Zöhling <Michael.Zoehling@outlook.com>
2019-03-01 14:27:16 +0100

    Lib und Deployment

--- MeinProgramm/MeinProgramm/Program.cs
+++ MeinProgramm/MeinProgramm/Program.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using MeineLib;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace MeinProgramm
 {
@@ -10,6 +12,13 @@ namespace MeinProgramm
     {
         static void Main(string[] args)
         {
+#if DEBUG
+            Console.WriteLine("Ich bin im DEBUG-Modus");
+#endif
+
+            var erg = Taschenrechner.Add(12, 3);
+            Console.WriteLine(erg);
+
             Console.WriteLine("---ENDE---");
             Console.ReadKey();
         }
